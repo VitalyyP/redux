64ab237b9db58ff97c2b3c82f74b0c58153ab2fa
VitalyyP <wisart@gmail.com>
2021-11-10 18:47:10 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { useDispatch, useSelector } from "react-redux";
+
 function App() {
+  const dispath = useDispatch();
+  const cash = useSelector(state => state.cash);
+  
+  const addCash = cash => {
+  dispatch({type:"ADD_CASH", payload: cash});
+}
+
+const getCash = cash => {
+  dispatch({type:"GET_CASH", payload: cash});
+}
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'app'}>
+      <div style={{fontSize:"3rem"}}>{cash}</div>
+      <div style={{display:"flax"}}>
+        <button onClick={()=>addCash(Nubmer(prompt()))}>Пополнить счет</button>
+        <button onClick={()=>getCash(Number(prompt()))}>Снять со счета</button>        
+      </div>
     </div>
   );
 }
